62ccb2ceaf64c5227add7d82dc6c5922172b428a
Kalyan Mazumder <kalyaninform@gmail.com>
2020-02-29 00:17:43 +0530

    initial commit with login form

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:gagan_invoice_management/welcomePage.dart';
+
+void main() =>  runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Invoice Management',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: WelcomePage(title: 'Flutter Login'),
+    );
+  }
+
+}
+
